905d4b8e4a8ae0c19f43043ef0a567d3e5a05d85
Beata Tomczak <tma.btty@gmail.com>
2019-12-05 19:39:17 +0100

    Install Mongoose module and connect with MongoDB database

--- src/app.js
+++ src/app.js
@@ -5,3 +5,12 @@ const port = 3000
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+//Connect to Database MongoDB
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://cc-team-octoo:kJHJ8!iJJhj@cluster0-fxhbq.azure.mongodb.net/test?retryWrites=true&w=majority')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err))
+
