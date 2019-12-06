7accd0e1b072a36ed16d5b0ed4690c404f0af83c
Beata Tomczak <tma.btty@gmail.com>
2019-12-06 13:07:04 +0100

    Fix connection

--- src/app.js
+++ src/app.js
@@ -2,15 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/natours')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
-//Connect to Database MongoDB
-const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://cc-team-octoo:kJHJ8!iJJhj@cluster0-fxhbq.azure.mongodb.net/test?retryWrites=true&w=majority')
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...', err))
+
+
 
