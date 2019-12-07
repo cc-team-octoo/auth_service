66a956ebb0707547884889af0e643e255c0a5221
Mirosław Biedrowski <biedrowski.miroslaw@wp.pl>
2019-12-07 12:39:21 +0100

    Add userSchema and usersModel

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  mail: String,
+  admin: Boolean
+});
+
+const Users = mongoose.model("Users", userSchema);
+
+const users = new Users({
+  name: "Mirosław Biedrowski",
+  mail: "biedrowski.miroslaw@wp.pl",
+  admin: true
+});
