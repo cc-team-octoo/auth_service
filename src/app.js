b55ad3c5886b12f6adf6d8fcdf302e12a760eed9
Mirosław Biedrowski <biedrowski.miroslaw@wp.pl>
2019-12-09 23:13:48 +0100

    Fix bug

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true},
-  mail: { type: String, unique: true, required: true },
+  name: { type: String, unique: true, required: true},
+  // mail: { type: String, unique: true, required: true },
   password: { type: String, required: true},
   admin: Boolean
 });
@@ -17,7 +17,7 @@ const User = mongoose.model("User", userSchema);
 
 const user = new User({
   name: "Mirosław Biedrowski",
-  mail: "biedrowski.miroslaw@wp.pl",
+  // mail: "biedrowski.miroslaw@wp.pl",
   password: "testowe123!"
   admin: true
 });
