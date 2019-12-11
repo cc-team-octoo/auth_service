344cede9033a2f626f820ebac63c59cb59348bf5
Mirosław Biedrowski <biedrowski.miroslaw@wp.pl>
2019-12-11 20:07:32 +0100

    Add FN validateUser

--- src/app.js
+++ src/app.js
@@ -1,23 +1,46 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const Joi = require("joi");
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, unique: true, required: true },
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
   // mail: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, minlength: 6, maxlength: 100 },
   admin: Boolean
 });
 
 const User = mongoose.model("User", userSchema);
 
-const user = new User({
-  name: "Mirosław Biedrowski",
-  // mail: "biedrowski.miroslaw@wp.pl",
-  password: "testowe123!",
-  admin: true
-});
+// const user = new User({
+//   name: "Mirosław Biedrowski",
+//   // mail: "biedrowski.miroslaw@wp.pl",
+//   password: "Testowe123!",
+//   admin: true
+// });
+
+function validateUser(user) {
+  const schema = Joi.object().keys({
+    name: Joi.string()
+      .min(8)
+      .max(50)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(6)
+      .required()
+      .max(20)
+      .regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,1024}$/) //special/number/capital
+  });
+  return Joi.validate(user, schema);
+}
