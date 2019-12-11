d69a9c5c67de6719635195b48803d4764060eb38
Aimekram <janina.byszewska@gmail.com>
2019-12-11 22:16:22 +0100

    Validate token and export middleware function

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,24 @@
+require('dotenv').config()
 const jwt = require('jsonwebtoken');
 
 
 function auth(req, res, next) {
     const token = req.header('x-auth-token');
-    if(!token) res.status(401).send('Access denied. No roken provided');
 
-    jwt.verify(token, )
+    //check if token
+    if(!token) return res.status(401).send('Access denied. No token provided');
+
+    //validate token
+    try {
+        const decoded = jwt.verify(token, process.env.AUTH_KEY); 
+        req.user = decoded;
+        next();
+    } 
+    //if token not valid catch exception(ex)
+    catch(ex) {
+        res.status(400).send('Invalid token')
+    }
+
 };
+
+module.exports = auth;
