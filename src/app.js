c3aabec575dc92798c5afac2bf798c3a4ad95afb
Mirosław Biedrowski <biedrowski.miroslaw@wp.pl>
2019-12-12 08:31:33 +0100

    Add TimeStamp

--- src/app.js
+++ src/app.js
@@ -6,18 +6,21 @@ app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    unique: true,
-    required: true,
-    minlength: 5,
-    maxlength: 50
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: true,
+      minlength: 5,
+      maxlength: 50
+    },
+    // mail: { type: String, unique: true, required: true },
+    password: { type: String, required: true, minlength: 6, maxlength: 100 },
+    admin: Boolean
   },
-  // mail: { type: String, unique: true, required: true },
-  password: { type: String, required: true, minlength: 6, maxlength: 100 },
-  admin: Boolean
-});
+  { timestamps: { createdAt: "created_at" } }
+);
 
 const User = mongoose.model("User", userSchema);
 
