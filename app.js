0319ad6a303e7df6e093bd886217daf79d255e9d
Aimekram <janina.byszewska@gmail.com>
2019-12-18 19:02:54 +0100

    Add options into mongoose.connect to get rid of deprcation warnings

--- app.js
+++ app.js
@@ -26,7 +26,9 @@ app.use(express.static(__dirname + '/public'));
 
 //database connection
 mongoose.connect(process.env.DB_CONNECTION, {
-        useNewUrlParser: true
+        useNewUrlParser: true, 
+        useUnifiedTopology: true, 
+        useCreateIndex: true
     }, )
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
