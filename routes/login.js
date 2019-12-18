77ba796c3b1694f1aafe1807fe1bf8846e8235ad
Aimekram <janina.byszewska@gmail.com>
2019-12-18 14:05:18 +0100

    Added bcrypt to login.js

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const config = require("config");
 const User = require('../models/user-model');
 
