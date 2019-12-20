06e3fdefe659360149f5f3c0dc31f7808f6c002a
Aimekram <janina.byszewska@gmail.com>
2019-12-20 16:06:43 +0100

    Separate users route from app.js

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const auth = require('./middleware/auth');
 const passportSetup = require('./config/passport-setup');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const Joi = require("joi");
 const methodOverride = require('method-override');
 require('dotenv/config');
 
@@ -21,17 +19,19 @@ const signup = require('./routes/signup');
 const admin = require('./routes/admin');
 const authRoutes = require('./routes/auth-routes');
 const profileRoutes = require('./routes/profile-routes');
+const users = require('./routes/users');
 
 
 // set the default templating engine to ejs
 app.set('view engine', 'ejs');
 
 //set up body parser to read request's body and cookies
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(cookieParser());
+
+//enable delete method in HTML
+app.use(methodOverride('_method'));
 
 // set up for static files like css
 app.use(express.static(__dirname + '/public'));
@@ -54,7 +54,8 @@ mongoose.connect(process.env.DB_CONNECTION, {
     }, )
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
-app.use(methodOverride('_method'));
+
+
 //use routes
 app.use('/', main);
 app.use('/login', login);
@@ -63,30 +64,7 @@ app.use('/signup', signup);
 app.use('/admin', admin);
 app.use('/auth', authRoutes);
 app.use('/profile', profileRoutes);
-
-
-app.put("/user/:id", auth, async (req, res) => {
-    const schema = {
-        name: Joi.string().min(5).max(50).required(),
-    }
-    const result = Joi.validate(req.body, schema);
-    if (result.error) {
-        return res.status(400).send(result.error.details[0].message)
-    };
-    const user = await User.findByIdAndUpdate(req.params.id, {
-        name: req.body.name
-    }, {
-        new: true
-    });
-    if (!user) return res.status(404).send("Cannot find user");
-    res.redirect("/admin");
-});
-
-app.delete('/user/:id', auth, async (req, res) => {
-    const user = await User.findByIdAndRemove(req.params.id);
-    if (!user) return res.status(404).send("Cannot find user");
-    res.redirect("/admin");
-});
+app.use('/users', users)
 
 //PORT listening
 const port = process.env.PORT || 8000;
