e9eafa1ecab578bc0e19be5376cab57e7725de6a
Aimekram <janina.byszewska@gmail.com>
2019-12-20 10:34:42 +0100

    Send token in cookies

--- routes/admin.js
+++ routes/admin.js
@@ -4,12 +4,18 @@ const auth = require("../middleware/auth");
 const User = require('../models/user-model');
 
 router.get('/', auth, async (req, res) => {
+    const token = req.cookies.token || '';
     const title = "Admin Page";
     const userName = req.user.name;
     const userId = req.user.id;
     const userList = await User.find();
-    console.log(userName, userId)
-    res.render('pages/admin', { title, userList, userName, userId })
+    console.log(userName, token)
+
+    res.cookie('token', token, {
+        expires: new Date(Date.now() + 1000),
+        secure: false, // set to true if your using https
+        httpOnly: true,
+    }).render('pages/admin', { title, userList, userName, userId })
 });
 
 module.exports = router;
